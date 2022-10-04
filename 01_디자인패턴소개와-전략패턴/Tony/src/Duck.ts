460e78797fb9f4b1d0a398396b497b71e2c69730
Tony <gth1123@naver.com>
2022-10-05 00:31:31 +0900

    tony-1 : 상속의 부작용

--- 01_디자인패턴소개와-전략패턴/Tony/src/Duck.ts
+++ 01_디자인패턴소개와-전략패턴/Tony/src/Duck.ts
@@ -32,3 +32,15 @@ class RedheadDuck extends Duck {
     console.log("붉은 머리 오리");
   }
 }
+
+class RubberDuck extends Duck {
+  constructor() {
+    super();
+  }
+  quack() {
+    console.log("삑삑");
+  }
+  display() {
+    console.log("고무 오리");
+  }
+}
