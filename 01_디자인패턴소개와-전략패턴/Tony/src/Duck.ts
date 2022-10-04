453feb7046cb9af0657180f11947dda4a7d1663d
Tony <gth1123@naver.com>
2022-10-05 00:07:30 +0900

    tony : SimUduck default

--- 01_디자인패턴소개와-전략패턴/Tony/src/Duck.ts
+++ 01_디자인패턴소개와-전략패턴/Tony/src/Duck.ts
@@ -1,3 +1,31 @@
 class Duck {
-  // 정리 예시
+  constructor() {}
+
+  quack() {
+    console.log("꽥꽥");
+  }
+  swim() {
+    console.log("수영");
+  }
+  display() {
+    console.log("오리");
+  }
+}
+
+class MallardDuck extends Duck {
+  constructor() {
+    super();
+  }
+  display() {
+    console.log("청둥 오리");
+  }
+}
+
+class RedheadDuck extends Duck {
+  constructor() {
+    super();
+  }
+  display() {
+    console.log("붉은 머리 오리");
+  }
 }
