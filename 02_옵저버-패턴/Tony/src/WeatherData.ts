c307781062756e850497d35b8574887ea153cc32
Tony <gth1123@naver.com>
2022-10-14 21:52:53 +0900

    tony-2: WeatherData 목표

---
+++ 02_옵저버-패턴/Tony/src/WeatherData.ts
@@ -0,0 +1,19 @@
+class WeatherData {
+  // 가장 최근에 측정된 온도, 습도, 기압 값을 리턴하는 메소드
+  getTemperature() {
+    // ...
+  }
+  getHumidity() {
+    // ...
+  }
+  getPressure() {
+    // ...
+  }
+
+  /**
+   * 기상 관측값이 갱신될 때마다 이 메소드가 호출됩니다
+   */
+  measurementsChanged() {
+    // 디스플레이를 업데이트하는 코드
+  }
+}
