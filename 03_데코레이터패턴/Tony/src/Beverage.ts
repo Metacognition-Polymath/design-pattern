d86198a2de2231ba3b590252740cfcb8e74ad0d3
Tony <gth1123@naver.com>
2022-10-22 14:25:48 +0900

    tony-3 : 데코레이터 패턴 학습

--- 03_데코레이터패턴/Tony/src/Beverage.ts
+++ 03_데코레이터패턴/Tony/src/Beverage.ts
@@ -1,109 +1,111 @@
-// Beverage 클래스에 우유, 두유, 모카, 휘핑 크림 첨가 여부를 보여주는 인스턴스 변수를 추가
-abstract class Beverage {
-  protected description: string = "스타버즈 음료";
-  protected milk: boolean = false;
-  protected soy: boolean = false;
-  protected mocha: boolean = false;
-  protected whip: boolean = false;
-  constructor() {}
+{
+  // Beverage 클래스에 우유, 두유, 모카, 휘핑 크림 첨가 여부를 보여주는 인스턴스 변수를 추가
+  abstract class Beverage {
+    protected description: string = "스타버즈 음료";
+    protected milk: boolean = false;
+    protected soy: boolean = false;
+    protected mocha: boolean = false;
+    protected whip: boolean = false;
+    constructor() {}
 
-  getDEscription() {
-    return this.description;
-  }
-
-  /**
-   * default cost : 1000
-   * milk : 100
-   * soy : 100
-   * mocha : 100
-   * whip : 100
-   */
-  cost() {
-    let cost = 1000;
-    if (this.hasMilk()) {
-      cost += 100;
+    getDEscription() {
+      return this.description;
     }
-    if (this.hasSoy()) {
-      cost += 100;
+
+    /**
+     * default cost : 1000
+     * milk : 100
+     * soy : 100
+     * mocha : 100
+     * whip : 100
+     */
+    cost() {
+      let cost = 1000;
+      if (this.hasMilk()) {
+        cost += 100;
+      }
+      if (this.hasSoy()) {
+        cost += 100;
+      }
+      if (this.hasMocha()) {
+        cost += 100;
+      }
+      if (this.hasWhip()) {
+        cost += 100;
+      }
+      return cost;
     }
-    if (this.hasMocha()) {
-      cost += 100;
+
+    hasMilk() {
+      return this.milk;
     }
-    if (this.hasWhip()) {
-      cost += 100;
+    setMilk(milk: boolean) {
+      this.milk = milk;
     }
-    return cost;
+
+    hasSoy() {
+      return this.soy;
+    }
+    setSoy(soy: boolean) {
+      this.soy = soy;
+    }
+
+    hasMocha() {
+      return this.mocha;
+    }
+    setMocha(mocha: boolean) {
+      this.mocha = mocha;
+    }
+
+    hasWhip() {
+      return this.whip;
+    }
+    setWhip(whip: boolean) {
+      this.whip = whip;
+    }
   }
 
-  hasMilk() {
-    return this.milk;
-  }
-  setMilk(milk: boolean) {
-    this.milk = milk;
+  class HouseBlend extends Beverage {
+    constructor() {
+      super();
+      this.description = "하우스 블렌드 커피";
+    }
+
+    cost() {
+      return super.cost() + 50;
+    }
   }
 
-  hasSoy() {
-    return this.soy;
-  }
-  setSoy(soy: boolean) {
-    this.soy = soy;
+  class DarkRoast extends Beverage {
+    constructor() {
+      super();
+      this.description = "다크 로스트 커피";
+    }
+
+    cost() {
+      return super.cost() + 60;
+    }
   }
 
-  hasMocha() {
-    return this.mocha;
-  }
-  setMocha(mocha: boolean) {
-    this.mocha = mocha;
+  class Decaf extends Beverage {
+    constructor() {
+      super();
+      this.description = "디카페인 커피";
+    }
+
+    cost() {
+      return super.cost() + 70;
+    }
   }
 
-  hasWhip() {
-    return this.whip;
-  }
-  setWhip(whip: boolean) {
-    this.whip = whip;
-  }
-}
+  class Espresso extends Beverage {
+    constructor() {
+      super();
+      this.description = "에스프레소";
+    }
 
-class HouseBlend extends Beverage {
-  constructor() {
-    super();
-    this.description = "하우스 블렌드 커피";
-  }
-
-  cost() {
-    return super.cost() + 50;
-  }
-}
-
-class DarkRoast extends Beverage {
-  constructor() {
-    super();
-    this.description = "다크 로스트 커피";
-  }
-
-  cost() {
-    return super.cost() + 60;
-  }
-}
-
-class Decaf extends Beverage {
-  constructor() {
-    super();
-    this.description = "디카페인 커피";
-  }
-
-  cost() {
-    return super.cost() + 70;
-  }
-}
-
-class Espresso extends Beverage {
-  constructor() {
-    super();
-    this.description = "에스프레소";
-  }
-
-  cost() {
-    return super.cost() + 80;
+    cost() {
+      return super.cost() + 80;
+    }
   }
 }
