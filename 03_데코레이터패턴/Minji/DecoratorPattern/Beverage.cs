415cf1a3c499c3faf27e40b79e84453291193bb1
jjadekwon <jjade.kwon@gmail.com>
2022-10-23 12:09:17 +0900

    minji : Decorator Pattern

---
+++ 03_데코레이터패턴/Minji/DecoratorPattern/Beverage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DecoratorPattern
+{
+    public enum BeverageSize { Tall, Grande, Venti };
+
+    public abstract class Beverage
+    {
+        protected string description = "";
+        public BeverageSize Size { get; set; } = BeverageSize.Tall;
+
+        public virtual void SetSize(BeverageSize size)
+        {
+            this.Size = size;
+        }
+
+        public virtual string GetDescription()
+        {
+            return $"({Size.ToString()}) {description}";
+        }
+
+        public abstract double Cost();
+    }
+
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            this.description = "에스프레소";
+        }
+
+        public override double Cost()
+        {
+            switch (Size)
+            {
+                case BeverageSize.Grande:
+                    return 2.99;
+                case BeverageSize.Venti:
+                    return 3.99;
+            }
+
+            return 1.99;
+        }
+    }
+
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            this.description = "하우스 블렌드 커피";
+        }
+
+        public override double Cost()
+        {
+            switch (Size)
+            {
+                case BeverageSize.Grande:
+                    return 1.89;
+                case BeverageSize.Venti:
+                    return 2.89;
+            }
+
+            return .89;
+        }
+    }
+
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            this.description = "다크 로스트 커피";
+        }
+
+        public override double Cost()
+        {
+            switch (Size)
+            {
+                case BeverageSize.Grande:
+                    return 1.90;
+                case BeverageSize.Venti:
+                    return 2.90;
+            }
+
+            return .90;
+        }
+    }
+}
---
+++ 03_데코레이터패턴/Minji/DecoratorPattern/Condiment/Condiment.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DecoratorPattern
+{
+    public abstract class CondimentDecorator : Beverage
+    {
+        protected Beverage beverage;
+
+        public override void SetSize(BeverageSize size)
+        {
+            this.beverage.Size = size;
+        }
+
+        public CondimentDecorator(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+    }
+
+    public class Mocha : CondimentDecorator
+    {
+        public Mocha(Beverage beverage) : base(beverage) { }
+
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 모카";
+        }
+
+        public override double Cost()
+        {
+            switch (beverage.Size)
+            {
+                case BeverageSize.Grande:
+                    return beverage.Cost() + 1.20;
+                case BeverageSize.Venti:
+                    return beverage.Cost() + 2.20;
+            }
+
+            return beverage.Cost() + .20;
+        }
+    }
+
+    public class Whip : CondimentDecorator
+    {
+        public Whip(Beverage beverage) : base(beverage) { }
+
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 휘핑크림";
+        }
+
+        public override double Cost()
+        {
+            switch (beverage.Size)
+            {
+                case BeverageSize.Grande:
+                    return beverage.Cost() + 1.14;
+                case BeverageSize.Venti:
+                    return beverage.Cost() + 2.14;
+            }
+
+            return beverage.Cost() + .14;
+        }
+    }
+
+    public class Soy : CondimentDecorator
+    {
+        public Soy(Beverage beverage) : base(beverage) { }
+
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 두유";
+        }
+
+        public override double Cost()
+        {
+            switch (beverage.Size)
+            {
+                case BeverageSize.Grande:
+                    return beverage.Cost() + 1.3;
+                case BeverageSize.Venti:
+                    return beverage.Cost() + 2.3;
+            }
+
+            return beverage.Cost() + .3;
+        }
+    }
+}
